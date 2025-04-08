292ff26f2a77f10fe6ea546b978e3e333ee8ccc7
CHANDAN S GOUDA <99399054+cfrohes@users.noreply.github.com>
2025-04-08 13:00:30 +0530

    Update and rename SalessavvyApplicationTests.java to SalessmartsApplicationTests.java

--- src/test/java/com/example/demo/SalessmartsApplicationTests.java
+++ src/test/java/com/example/demo/SalessmartsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.salesSmarts1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SalessavvyApplicationTests {
+class SalessmartsApplicationTests {
 
 	@Test
 	void contextLoads() {
